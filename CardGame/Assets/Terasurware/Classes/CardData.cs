d9a5467f9d30337e292eedcc1592330557549506
mmeo009 <shh020615@gmail.com>
2023-09-11 01:57:13 +0900

    카드 데이터 엑셀 추가 및 util 스크립트 생성

---
+++ CardGame/Assets/Terasurware/Classes/CardData.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CardData : ScriptableObject
+{	
+	public List<Param> param = new List<Param> ();
+
+	[System.SerializableAttribute]
+	public class Param
+	{
+		
+		public double index;
+		public string id;
+		public string name;
+		public string method;
+		public double type;
+		public string element;
+		public double attack_type;
+		public double cost;
+		public string rarity;
+		public string text;
+		public string comment;
+		public string cc_1;
+		public string cc_2;
+		public double stat_01;
+		public double stat_02;
+		public double stat_03;
+		public double stat_04;
+		public string image;
+	}
+}
